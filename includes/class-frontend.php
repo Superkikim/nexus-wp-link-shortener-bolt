e4768bfbc8b56794217e965a27013ce7dc9ccc52
Superkikim <superkikim@sissaoui.com>
2025-08-29 01:16:56 +0200

    Removed URL cleaning

--- includes/class-frontend.php
+++ includes/class-frontend.php
@@ -60,10 +60,7 @@ class Nexus_Links_Frontend {
         
         // Determine target URL
         $target_url = $this->get_target_url($link);
-        
-        // Clean the target URL to remove any unwanted parameters
-        $target_url = $this->clean_target_url($target_url);
-        
+
         // Perform redirect
         wp_redirect($target_url, $link->http_status);
         exit;
@@ -82,46 +79,7 @@ class Nexus_Links_Frontend {
         return get_permalink($link->post_id);
     }
     
-    /**
-     * Clean target URL to remove unwanted parameters
-     */
-    private function clean_target_url($url) {
-        // Parse the URL
-        $parsed_url = parse_url($url);
-        
-        if (!$parsed_url) {
-            return $url;
-        }
-        
-        // If there are query parameters, filter out unwanted ones
-        if (isset($parsed_url['query'])) {
-            parse_str($parsed_url['query'], $query_params);
-            
-            // Remove lang parameter if it's the default 'en'
-            if (isset($query_params['lang']) && $query_params['lang'] === 'en') {
-                unset($query_params['lang']);
-            }
-            
-            // Rebuild the URL
-            $clean_query = http_build_query($query_params);
-            $parsed_url['query'] = $clean_query;
-            
-            // If no query parameters left, remove the query component
-            if (empty($clean_query)) {
-                unset($parsed_url['query']);
-            }
-        }
-        
-        // Rebuild the URL
-        $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-        $host = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-        $port = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-        $path = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-        $query = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
-        
-        return $scheme . $host . $port . $path . $query . $fragment;
-    }
+
     
     /**
      * Record click analytics
